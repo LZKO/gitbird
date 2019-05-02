a950a1d95a8d1759811ca9ea6f7481e860d843aa
liyunyao <1666927605@qq.com>
2019-05-02 17:26:21 +0800

    git test

---
+++ exam/GitUseTest.java
@@ -0,0 +1,15 @@
+package com.example.gitbird.exam; 
+
+/* *
+   * Created by LIYUNYAO on 2019/5/2
+   * Time: 17:25
+   * Year: 2019
+   */
+
+public class GitUseTest {
+
+    private void m1() {
+        System.out.println("11111");
+    }
+
+}
