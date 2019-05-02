9816dcb17a605bd91231104cb930826b9f6b22ad
liyunyao <1666927605@qq.com>
2019-05-02 17:21:58 +0800

    init

---
+++ GitbirdApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitbird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitbirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitbirdApplication.class, args);
+	}
+
+}
